5ad399912cbd8a5e8501d9e0df5ca145aa6a0b0c
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-05-20 14:03:00 +0300

    Set

---
+++ src/com/company/HeavenlyBody.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satelities;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satelities = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatelities() {
+        return new HashSet<>(this.satelities);
+    }
+
+    public boolean addMoon(HeavenlyBody moon){
+        return satelities.add(moon);
+    }
+}
